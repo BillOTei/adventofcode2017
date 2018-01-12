8c9c9084914dce50e14270979b1b8ca12c9a8e64
Alexandre Teilhet <a.teilhet.ext@pro.neo-soft.fr>
2018-01-12 17:48:16 +0100

    day 10 part 2

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -1,6 +1,6 @@
 object Day10 {
   def main(args: Array[String]): Unit = {
-    println(day10Part1("102,255,99,252,200,24,219,57,103,2,226,254,1,0,69,216"))
+    println(day10Part2("102,255,99,252,200,24,219,57,103,2,226,254,1,0,69,216"))
   }
 
   def day10Part1(s: String): Int = {
@@ -12,6 +12,21 @@ object Day10 {
     go(0, 0, (0 to 255).toList, s.split(",").toList.map(_.toInt))
 //    go(0, 0, (0 to 4).toList, List(3, 4, 1, 5))
   }
+
+  def day10Part2(s: String): String = {
+    def go(round: Int, pos: Int, skip: Int, l: List[Int], lengths: List[Int]): String = lengths match {
+      case Nil if round >= 65 =>
+        l.grouped(16).map(_.reduceLeft(_ ^ _)).foldLeft("")((acc, v) => {
+          if (v.toHexString.length < 2) acc + "0" + v.toHexString else acc + v.toHexString
+        })
+      case Nil if round < 65 => go(round + 1, pos, skip, l, getASCIILengths(s))
+      case h :: tail => go(round, (pos + h + skip) % l.length, skip + 1, getNewList(l, pos, h), tail)
+    }
+
+    go(1, 0, 0, (0 to 255).toList, getASCIILengths(s))
+  }
+
+  def getASCIILengths(s: String): List[Int] = s.toCharArray.map(_.toInt).toList ++ List(17, 31, 73, 47, 23)
 
   def getNewList(l: List[Int], start: Int, length: Int): List[Int] = {
     val zipped = l.zipWithIndex
