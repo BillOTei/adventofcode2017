452afdb1e95b76e8c8516247cd91dccb06405d9e
Alexandre Teilhet <a.teilhet.ext@pro.neo-soft.fr>
2018-01-22 17:41:29 +0100

    day 11 part 2

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -1,13 +1,21 @@
 object Day11 {
   def main(args: Array[String]): Unit = {
-    println(day11Part1("ne,nw,se,nw,ne,s,s,s,sw,ne"))
+    println(day11Part2("ne,nw,se,nw,ne,s,s,s,sw,ne,sw,sw"))
   }
 
   def day11Part1(s: String): Int = {
     val pos = s.split(",").map(s => Step.withName(s.trim)).foldLeft((0, 0))((p, m) => Step.move(m, p))
 
-    List(Math.abs(pos._1), Math.abs(pos._2), Math.abs(pos._2 - pos._1)).max
+    getSteps(pos)
   }
+
+  def day11Part2(s: String): Int = {
+    s.split(",").map(s => Step.withName(s.trim)).scanLeft((0, 0))((p, m) => {
+      Step.move(m, p)
+    }).map(getSteps).max
+  }
+
+  def getSteps(pos: (Int, Int)): Int = List(Math.abs(pos._1), Math.abs(pos._2), Math.abs(pos._2 - pos._1)).max
 
   object Step extends Enumeration {
     type Step = Value
