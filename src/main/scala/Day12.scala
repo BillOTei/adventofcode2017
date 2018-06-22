2c12b181ec707c26fbbb1cbaad4dcd694f37a40e
billotei <ateilhet@gmail.com>
2018-06-22 13:33:37 +0200

    day 12 part 1

--- src/main/scala/Day12.scala
+++ src/main/scala/Day12.scala
@@ -1,25 +1,52 @@
+import scala.collection.mutable.ArrayBuffer
+
 object Day12 {
   def main(args: Array[String]): Unit = {
     println(day12Part1("0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5"))
   }
 
-  case class Elmt(v: Int, children: List[Elmt])
-
+  /**
+    * Not very nicely done, quite imperative and involving buffers
+    * @param s the input
+    * @return
+    */
   def day12Part1(s: String): Int = {
+    // Keep raw data as array for faster index look up
     val rawData = s.split("\n").map(p => {
       val pData = p.split(" <-> ")
-      // Data should be consistent
+      // Zip with idx like
       (pData(0).toInt, pData(1).split(", ").toList.map(_.toInt))
     })
 
-    val t = rawData.map(t => (t._1, t._2.map(c => rawData.find(_._1 == c).get)))
+    // First buffer to store a reference of the children elements of the ones directly connected to 0
+    val b = ArrayBuffer[Int]()
 
+    // First and second results, directly connected to 0 elements and their children
+    val r = rawData.foldLeft(Nil: List[Int])((acc, e) => {
+      if (acc.isEmpty) List(0) ++ e._2
+      else if (acc.contains(e._1)) {
+        b ++= e._2
 
+        acc
+      } else if (e._2.intersect(acc).nonEmpty) acc ++ List(e._1)
+      else acc
+    })
+    val r1 = (b filter(_ != 0) distinct).toList
 
-    0
-  }
+    // Look up in the children result for all the children of children, could be recursive
+    val b1 = r1.toBuffer
+    val b2 = ArrayBuffer[Int]()
+    while (b1.nonEmpty) {
+      b1.foreach(v => {
+        val toAddAndCheck = rawData.find(_._1 == v).get._2.filterNot(i => r.contains(i) || b2.contains(i))
+        b1 ++= toAddAndCheck
+        b2 ++= toAddAndCheck
+        b1 -= v
+      })
+    }
 
-  def getElmt(v: (Int, List[Int]), data: List[(Int, List[Int])]): Elmt = {
-    Elmt(v._1, v._2.map(c => getElmt(data.find(_._1 == c).get, data)))
+    val r2 = b2.toList
+
+    (r ++ r1 ++ r2 distinct).length
   }
 }
